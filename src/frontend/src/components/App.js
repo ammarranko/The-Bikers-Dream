c583f8bead789078b634f2128889fc6486bf703b
Ammar Ranko <ammar.ranko707@gmail.com>
2025-10-07 11:51:16 -0400

    add basic React Router setup to app.js

--- src/frontend/src/components/App.js
+++ src/frontend/src/components/App.js
@@ -2,12 +2,32 @@ import logo from './assets/logo.png';
 import './App.css';
 import Header from './Header.jsx';
 import Auth from './auth/auth.jsx';
+import Home from './home/Home.jsx';
+import ProtectedRoute from './ProtectedRoute.jsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Test from "./test/Test";
 
 function App() {
   return (
-    <div >
-      <Auth/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Auth />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/Test" element={
+          <ProtectedRoute>
+            <Test />
+          </ProtectedRoute>
+        } />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
