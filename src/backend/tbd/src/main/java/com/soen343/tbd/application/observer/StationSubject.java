2cfa69abd621c0b5eef3d1b9c23d4c4cc365c49a
elif5446 <elifsagsesen@gmail.com>
2025-10-23 20:38:40 -0400

    Real time updates across users - implemented w/ observer pattern

---
+++ src/backend/tbd/src/main/java/com/soen343/tbd/application/observer/StationSubject.java
@@ -0,0 +1,14 @@
+package com.soen343.tbd.application.observer;
+
+// Observer package in application layer because it's part of app's business logic
+
+import com.soen343.tbd.application.dto.StationDetailsDTO;
+
+// Define contracts for Publisher (Subject) in Observer pattern
+public interface StationSubject {
+    void attach(StationObserver observer); // Subscribe to updates
+
+    void detach(StationObserver observer); // Unsubscribe from updates
+
+    void notifyObservers(StationDetailsDTO station); // Broadcast updates to all subscribers
+}
